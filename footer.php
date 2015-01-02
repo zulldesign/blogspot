ab89677f38db07f455275a1b1dfd1d7fe54e1d91
zulldesign <admin@zulldesign.ml>
2015-01-02 19:19:37 +0800

    l

---
+++ footer.php
@@ -0,0 +1,10 @@
+<table border=0 cellspacing=0 cellpadding=0 width="100%" align="center" bgcolor="#cccccc">
+	<tr>
+		<td>
+			<font size=1 color=black>
+				<p align="center">Script developed by <a href="http://www.monitor-line.com" target="_blank">Monitor-Line Infoservices Private Limited</a>  </p>
+			</font>
+		</td>
+	</tr>
+
+</table>
---
+++ topbanner.php
@@ -0,0 +1,1 @@
+<!-- Top banner panel -->
