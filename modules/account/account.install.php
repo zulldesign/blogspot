1e0431da43f85df905d645fd6f9cce948de2645f
zulldesign <admin@zulldesign.ml>
2015-01-05 14:58:32 +0800

    l

--- modules/account/account.install.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* ############################################################ *\
- ----------------------------------------------------------------
-@package	Jcow Social Networking Script.
-@copyright	Copyright (C) 2009 - 2010 jcow.net.  All Rights Reserved.
-@license	see http://jcow.net/license
- ----------------------------------------------------------------
-\* ############################################################ */
-include 'includes/libs/account.install.module.php';
--- modules/admin/inc/update.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include 'includes/libs/admin_update.module.php';
